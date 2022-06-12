2d2b0b35927913b35d1ef8fa1a55a069ae282560
Eric <eric.w.k.wong@outlook.com>
2022-06-12 02:24:11 -0400

    updated sniffer with argument parser

--- src/packet_sniffer.py
+++ src/packet_sniffer.py
@@ -1,7 +1,30 @@
+import argparse
 from scapy.all import *
 
-def handler(packet):
-    print(packet.summary())
+class Sniffer:
+    # Defining constructor
+    def __init__(self, args):
+        self.args = args
+
+    # Defining __call__ method
+    def __call__(self, packet):
+        if self.args.verbose:
+            packet.show()
+        else:
+            print(packet.summary())
+    
+    def run_forever(self):
+        sniff(iface=self.args.interface, prn=self, store=0)
+
 
 if __name__ == "__main__":
-    sniff(iface="en0",prn=handler, store=0)
+    
+    # Configure arguments using argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', default=False, action='store_true', help='more')
+    parser.add_argument('-i', '--interface', type=str, required=True, help='network interface name')
+    args = parser.parse_args()
+
+    # Call sniffer
+    sniffer = Sniffer(args)
+    sniffer.run_forever()
